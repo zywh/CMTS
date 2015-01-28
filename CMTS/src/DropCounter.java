12c876b0d60f1b7e76af6753c495a183757f8e8a
dzheng <dzheng@labucs2>
2015-01-28 07:48:47 -0500

    First Change on Ubuntu Server

--- CMTS/src/DropCounter.java
+++ CMTS/src/DropCounter.java
@@ -24,8 +24,7 @@ public class DropCounter {
 
 	public void updateCounter(String key, long counter) {
 		
-		//Get Change
-		System.out.println("This is second version to GIT-slave");
+		
 		newcounter.put(key, counter);
 	}
 
